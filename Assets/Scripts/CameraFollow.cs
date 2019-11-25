291fd3e1c16e487d406b456fda5af61f890d232a
Santeri Huotari <santtu.huotari11@gmail.com>
2019-11-25 19:00:40 +0200

    Implement player and camera movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPlayer();
+    }
+
+    private void FollowPlayer()
+    {
+        Vector3 moveTarget = new Vector3(player.transform.position.x + 10f, player.transform.position.y + 3f, player.transform.position.z);
+
+        transform.position = moveTarget;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float baseSpeed = 10f;
+    [SerializeField]
+    private float jumpStrenght = 5f;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void Movement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 moveVector = new Vector3(-v, 0, h) * baseSpeed * Time.deltaTime;
+
+        rb.velocity += moveVector;
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpStrenght, ForceMode.Impulse);
+    }
+}
